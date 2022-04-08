cbb50f93cee3f2d03da1097c69c71ca0ee113532
iMaeGoo <3870622-imaegoo@users.noreply.gitlab.com>
2022-04-08 01:05:04 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,6 @@
 module.exports = require('twikoo-vercel')
+
+module.exports = async (requestArg, responseArg) => {
+  console.log(JSON.stringify(requestArg.headers))
+  return await require('twikoo-vercel')(requestArg, responseArg)
+}
