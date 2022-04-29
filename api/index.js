f6151426f83c8429db80368beba87642a4c940b2
iMaeGoo <3870622-imaegoo@users.noreply.gitlab.com>
2022-04-29 08:19:17 +0000

    Update api/index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 const regex = /^https:\/\/twikoo\.js\.org\/?$/
+const localhostRegex = /^https?:\/\/(localhost|127\.0\.0\.1|0\.0\.0\.0)(:\d{1,5})?\/?$/
 
 module.exports = async (requestArg, responseArg) => {
   if (regex.test(requestArg.headers.origin) && regex.test(requestArg.headers.referer)) {
+    return await require('twikoo-vercel')(requestArg, responseArg)
+  } else if (localhostRegex.test(requestArg.headers.origin) && localhostRegex.test(requestArg.headers.referer)) {
     return await require('twikoo-vercel')(requestArg, responseArg)
   } else {
     return responseArg.end('')
